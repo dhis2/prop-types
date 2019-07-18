3f0caf1547dc72bf0b623ea682bcef16500eec8d
Hendrik de Graaf <hendrik@dhis2.org>
2019-07-18 15:32:15 +0200

    docs: add and build documentation - Add jsDoc comments to prop-type functions - Create handlebars template to create README - Add build command to build README.md with documentation - Add husky hook to build docs on pre-commit (it's very fast)

--- src/instanceOfComponent.js
+++ src/instanceOfComponent.js
@@ -27,6 +27,28 @@ const instanceOfComponentFactory = (Component, isRequired) => (
     return null
 }
 
+/**
+ * Ensure the prop value is an instance of a certain component
+ * @param {function} Component - The component that is expected
+ * @return {Error|null} Returns null if all conditions are met, or an error
+ * @example
+ * import React from 'react'
+ * import { instanceOfComponent } from '@dhis2/prop-types'
+ * import { Button } from './Button'
+ *
+ * const ButtonWrap = ({ children }) => <div>{children}</div>
+ * // This would allow the ButtonWrap to be empty
+ * ButtonWrap.propTypes = {
+ *     children: instanceOfComponent(Button)
+ * }
+ *
+ * // Enforce presence of a Button instance
+ * ButtonWrap.propTypes = {
+ *     children: instanceOfComponent(Button).isRequired
+ * }
+ * @function
+ */
+
 export const instanceOfComponent = Component => {
     const fn = instanceOfComponentFactory(Component, false)
     fn.isRequired = instanceOfComponentFactory(Component, true)
--- src/mutuallyExclusive.js
+++ src/mutuallyExclusive.js
@@ -41,6 +41,34 @@ const mutuallyExclusiveFactory = (exlusivePropNames, propType, isRequired) => (
     return null
 }
 
+/**
+ * Ensure that only one property within a specified list is thruthy
+ * This function will also check if the current property value is of the specified type
+ * @param {array<string>} exlusivePropNames - The prop names to be checked
+ * @param {function} propType - The prop-type that the current prop-value needs to conform to
+ * @return {Error|null} Returns null if all conditions are met, or an error
+ * @example
+ * import React from 'react'
+ * import cx from 'classnames'
+ * import propTypes from 'prop-types'
+ * import { mutuallyExclusive } from '@dhis2/prop-types'
+ *
+ * const Alert = ({ danger, warning, success, children }) => (
+ *     <div className={cx({danger, warning, success})}>
+ *         {children}
+ *     </div>
+ * )
+ *
+ * const statusPropType = mutuallyExclusive(['danger', 'warning', 'success'], propTypes.bool)
+ *
+ * Alert.propTypes = {
+ *     children: propTypes.node,
+ *     danger: statusPropType,
+ *     warning: statusPropType,
+ *     success: statusPropType,
+ * }
+ * @function
+ */
 export const mutuallyExclusive = (exlusivePropNames, propType) => {
     const fn = mutuallyExclusiveFactory(exlusivePropNames, propType, false)
     fn.isRequired = mutuallyExclusiveFactory(exlusivePropNames, propType, true)
