754f13fec1ef713ccee59f79976fce7c0f39c1a4
Hendrik de Graaf <hendrik@dhis2.org>
2019-09-24 10:24:55 +0200

    fix(docs): build docs on pre-commit

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,8 +1,6 @@
 module.exports = {
     hooks: {
         'commit-msg': 'd2-style commit check',
-        'pre-commit': 'd2-style validate',
-        'pre-push':
-            'yarn docs && git add README.md && git commit --amend --no-edit',
+        'pre-commit': 'd2-style validate && yarn docs && git add README.md',
     },
 }
