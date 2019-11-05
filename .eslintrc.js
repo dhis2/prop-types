7b884a722e72cafdf2e79b9229ad0a20330ab9c2
Hendrik de Graaf <HendrikThePendric@users.noreply.github.com>
2019-11-05 10:40:34 +0100

    chore(eslint): extend from eslint:recommended (#66)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ const { config } = require('@dhis2/cli-style')
 
 module.exports = {
     parser: 'babel-eslint',
-    extends: [config.eslint],
+    extends: ['eslint:recommended', config.eslint],
     rules: {
         'max-params': ['error', { max: 5 }],
     },
