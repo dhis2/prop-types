05d427866f631c1eff38cb3c50d58afedd61a971
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2019-12-03 09:02:54 +0700

    chore: update config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,5 @@
 const { config } = require('@dhis2/cli-style')
 
 module.exports = {
-    parser: 'babel-eslint',
-    extends: ['eslint:recommended', config.eslint],
-    rules: {
-        'max-params': ['error', { max: 5 }],
-    },
+    extends: [config.eslint],
 }
