55a9f28003b700988b4b4020f7dc7aeff657e7fe
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2020-03-31 10:11:24 +0200

    fix(conditional): expose prop type publicly

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import propTypes from 'prop-types'
 
 import { arrayWithLength } from './arrayWithLength.js'
+import { conditional } from './conditional';
 import { instanceOfComponent } from './instanceOfComponent.js'
 import { mutuallyExclusive } from './mutuallyExclusive.js'
 import { requiredIf } from './requiredIf.js'
 
 propTypes.arrayWithLength = arrayWithLength
+propTypes.conditional = onditional
 propTypes.instanceOfComponent = instanceOfComponent
 propTypes.mutuallyExclusive = mutuallyExclusive
 propTypes.requiredIf = requiredIf
 
-export { arrayWithLength, instanceOfComponent, mutuallyExclusive, requiredIf }
+export {
+    arrayWithLength,
+    conditional,
+    instanceOfComponent,
+    mutuallyExclusive,
+    requiredIf,
+}
+
 export default propTypes
