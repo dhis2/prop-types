e7399c9fdfd8e7742a52b99c068497ca6d796707
Hendrik de Graaf <hendrik@dhis2.org>
2021-09-09 16:35:54 +0200

    chore: update library structure BREAKING CHANGE: we no longer append to and re-export the `PropTypes` object from the `prop-types` package. Instead it will need to be installed by the consumer and regular prop-types need to be imported from there. Only the DHIS2 custom prop-types can be imported from this package from now on.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
-import PropTypes from 'prop-types'
-import * as customPropTypes from './propTypes'
-
-/*
- * Export our own prop-types as named exports, this allows this import style:
- * import { arrayWithLength } from '@dhis2/prop-types'
- */
-export * from './propTypes'
-
-/*
- * Reexport the prop-types lib under a namespace, as is our convention. The
- * name is Pascalcased, this allows for this import style:
- * import { PropTypes } from '@dhis2/prop-types'
- */
-export { PropTypes }
-
-/*
- * Export custom prop-types as the default export. This only contains our
- * own prop-types. Import style is (naming is up to the user of course):
- * import dhis2PropTypes from '@dhis2/prop-types'
- */
-export default customPropTypes
+export { arrayWithLength } from './arrayWithLength.js'
+export { conditional } from './conditional'
+export { instanceOfComponent } from './instanceOfComponent.js'
+export { mutuallyExclusive } from './mutuallyExclusive.js'
+export { requiredIf } from './requiredIf.js'
